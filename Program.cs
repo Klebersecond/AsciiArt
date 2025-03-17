b4398a005a29d648c2c9f16ee514d28309149e33
Kleber <klebernssii@gmail.com>
2025-03-17 10:33:26 -0300

    finaliza programa

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+
+ 
+Console.WriteLine("================================================.");
+Console.WriteLine("     .-.   .-.     .--.                         |");
+Console.WriteLine("    | OO| | OO|   / _.-' .-.   .-.  .-.   .''.  |");
+Console.WriteLine("    |   | |   |   |  '-. '-'   '-'  '-'   '..'  |");
+Console.WriteLine("    '^^^' '^^^'    '--'                         |");
+Console.WriteLine("===============.  .-.  .================.  .-.  |");
+Console.WriteLine("               | |   | |                |  '-'  |");
+Console.WriteLine("               | |   | |                |       |");
+Console.WriteLine("               | ':-:' |                |  .-.  |");
+Console.WriteLine("l42            |  '-'  |                |  '-'  |");
+Console.WriteLine("==============='       '================'       |");
